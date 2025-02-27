718927cc3b04acb90bcf87e3029c72589530433d
kotturp <prasannakttur@gmail.com>
2025-02-27 01:29:02 -0600

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -24,7 +24,7 @@ const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
+            Software Engineering
           </h1>
         </motion.div>
 
@@ -33,12 +33,11 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              the best{" "}
+              Praneetha Mukkamala{" "}
             </span>
-            project exprience
+            Software Developer Engineer
           </span>
         </motion.div>
 
@@ -53,7 +52,7 @@ const HeroContent = () => {
           variants={slideInFromLeft(1)}
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
         >
-          Learn More!
+          Resume
         </motion.a>
       </div>
 
